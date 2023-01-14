c8784e1ec5c8a771e42d7c9b0f8e1990cc58b2fc
Air Quality And Related Topics <45898680+airqualityart@users.noreply.github.com>
2023-01-14 22:13:50 +0100

    Added the solution to day 04 of year 2022.

---
+++ year-2022/day-04/part-2.c
@@ -0,0 +1,62 @@
+/* Solution for part 2 of day 04 of Advent of Code (r) 2022.
+
+Copyright (c) 2023, Air Quality And Related Topics.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+  (1) Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+  (2) Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+  (3) The name of the author may not be used to endorse or promote products
+  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+
+Notes :
+
+ - This work is in no way a promotion for or an endorsement of Advent of Code
+   or any of its products or assets. I have no ties whatsoever with Advent of
+   Code. I just solve its puzzles for fun. Advent of Code is a registered
+   trademark in the United States of America. See the Advent of Code website
+   (https://adventofcode.com) for more information.
+
+ - See https://adventofcode.com/2022/day/4 for the description of the puzzle.
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include "commons.h"
+
+#define MAX_LINE_LENGTH 20
+
+int main() {
+    int eof, a, b, c, d, sum = 0;
+    char line[MAX_LINE_LENGTH];
+    do {
+        if (!readline(line, MAX_LINE_LENGTH, &eof)) {
+            printf("Could not read line\n");
+            return FALSE;
+        }
+        if (strlen(line) == 0) continue;
+        sscanf(line, "%d-%d,%d-%d", &a, &b, &c, &d);
+        if ((a >= c && a <= d) || (b >= c && b <= d) ||
+            (c >= a && c <= b) || (d >= a && d <= b)) sum++;
+    } while (!eof);
+    printf("%d\n", sum);
+    return TRUE;
+}
