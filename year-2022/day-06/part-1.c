48834e85eabde632644ea1b6f37d7bf54a78f4bb
Air Quality And Related Topics <45898680+airqualityart@users.noreply.github.com>
2023-01-14 22:15:48 +0100

    Added the solution to day 06 of year 2022.

---
+++ year-2022/day-06/part-1.c
@@ -0,0 +1,71 @@
+/* Solution for part 1 of day 06 of Advent of Code (r) 2022.
+
+Copyright (c) 2023, Air Quality And Related Topics.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+  (1) Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+  (2) Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+  (3) The name of the author may not be used to endorse or promote products
+  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+
+Notes :
+
+ - This work is in no way a promotion for or an endorsement of Advent of Code
+   or any of its products or assets. I have no ties whatsoever with Advent of
+   Code. I just solve its puzzles for fun. Advent of Code is a registered
+   trademark in the United States of America. See the Advent of Code website
+   (https://adventofcode.com) for more information.
+
+ - See https://adventofcode.com/2022/day/6 for the description of the puzzle.
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include "commons.h"
+
+#define MARKER_SIZE 4
+
+int all_different(char marker[]) {
+    /* Returns TRUE iff all characters of marker are different. */
+    char characters[MARKER_SIZE];
+    int i, j = 1;
+    for (i = 0; i < MARKER_SIZE; i++) characters[i] = marker[0];
+    for (i = 1; i < MARKER_SIZE; i++)
+        if (strchr(characters, marker[i]) == NULL)
+            characters[j++] = marker[i];
+        else
+            return FALSE;
+    return TRUE;
+}
+
+int main() {
+    char marker[MARKER_SIZE];
+    int index = 0, counter = 0, i;
+    for (i = 0; i < MARKER_SIZE; i++) marker[i] = '\0';
+    while ((marker[index++] = getchar()) != EOF) {
+        counter++;
+        if (index == MARKER_SIZE) index = 0;
+        if (all_different(marker)) break;
+    }
+    printf("%d\n", counter);
+    return TRUE;
+}
