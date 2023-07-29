d284438081d314ae6b422c189914098fc76557a7
Air Quality And Related Topics <45898680+airqualityart@users.noreply.github.com>
2023-07-29 12:28:36 +0200

    Updated the script that runs year 2022 solutions.

--- year-2022/run-solution.bash
+++ year-2022/run-solution.bash
@@ -2,10 +2,11 @@
 #
 # This script compiles and runs the solution to puzzle $1, part $2.
 #
-# Copyright (c) 2023, Air Quality And Related Topics.
+# Copyright (c) 2023-now, Air Quality And Related Topics.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
+# (BSD "3-clause" license):
 #
 #   (1) Redistributions of source code must retain the above copyright notice,
 #   this list of conditions and the following disclaimer.
@@ -36,6 +37,11 @@
 #
 # ./run-solution.bash 09 2
 #
+# Optionally, you can pass a third argument to specify the C compiler to use
+# (either gcc or clang, the default being gcc). For example:
+#
+# ./run-solution.bash 09 2 clang
+#
 # Before running the solution on the actual puzzle input, this script first
 # runs the solution on the test data, if any. Test data are files named:
 #
@@ -45,16 +51,35 @@
 # - the first line contains the answer to the given test input data.
 # - the rest of the file is the test input data.
 
+# Useful function(s)
+function message {
+    # Pretty-print first argument, and exit with error code if given.
+    #
+    # Usage:
+    #
+    # message "some text" [error_code]
+    echo ""
+    echo ">>> $1"
+    echo ""
+    [[ $# > 1 ]] && exit $2
+}
+
 # Quality checks on inputs
-if [ $# != 2 ]; then
-    echo ""
-    echo ">>> You must provide exactly two arguments."
-    echo ""
-    exit 1
+if [ $# != 2 ] && [ $# != 3 ] ; then
+    message "You must provide either two or three arguments." 1
 fi
 
 # Compiler and its options (we choose to be very strict with the code)
-COMPILE="gcc -ansi -Wall -Wextra -Wpedantic -Werror -pedantic-errors"
+COMPILER="gcc"
+[ $# = 3 ] && COMPILER=$3
+case $COMPILER in
+    gcc)
+        COMPILE="gcc -ansi -Wall -Wextra -Wpedantic -Werror -pedantic-errors";;
+    clang)
+        COMPILE="clang -ansi";;
+    *)
+        message "Unknown or unsupported compiler: $COMPILER." 2;;
+esac
 LIBS="-lm"
 
 # Directories and files
@@ -67,19 +92,15 @@ FILE_EXE="$DIR_SRC/$FILE_PFX.out"
 FILE_INPUT="$DIR_SRC/${FILE_PFX}_input-data.txt"
 
 # Compile
-if ! $COMPILE -I$DIR_INC $FILE_SRC $DIR_INC/commons.c $LIBS -o $FILE_EXE; then
-    echo ""
-    echo ">>> There were errors during compilation."
-    echo ""
-    exit 2
+$COMPILE -I$DIR_INC $FILE_SRC $DIR_INC/commons.c $LIBS -o $FILE_EXE
+if [ $? != 0 ] ; then
+    message "There were errors during compilation." 3
+else
+    message "Compiled successfully, running program..."
 fi
-echo ""
-echo ">>> Compiled successfully, running program..."
-echo ""
 
 # Run the tests
-echo ">>> Running the solution on test cases..."
-echo ""
+message "Running the solution on test cases..."
 for F in $DIR_SRC/${FILE_PFX}_test-data-*.txt; do
     if [ $F == "$DIR_SRC/${FILE_PFX}_test-data-*.txt" ]; then break; fi
     ANSWER_REAL=$(head -n 1 $F)
@@ -93,8 +114,7 @@ done
 echo ""
 
 # Run the solution on the actual puzzle input
-echo ">>> Running the solution on the actual puzzle input data..."
-echo ""
+message "Running the solution on the actual puzzle input data..."
 if [ -f $FILE_INPUT ]; then
     ANSWER=$($FILE_EXE < $FILE_INPUT)
     echo "    Answer = $ANSWER"
